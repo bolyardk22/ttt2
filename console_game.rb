d7837740f8acfcd67df0ebb060c7fbe0df77a782
bolyardk22 <collinsk22@mycu.concord.edu>
2017-09-29 16:15:07 -0400

    console human added

--- console_game.rb
+++ console_game.rb
@@ -35,4 +35,79 @@ class Console_game
 
 	def update_board
 		marker = active_player.marker
-		
+		move = get_move
+
+		if board.valid_position?(move)
+			board.update_position(move,marker)
+
+		else puts "Invalid move; please choose again."
+			update_board
+		end
+	end
+
+	def change_player
+		if active_player == player_1
+			@active_player = player_2
+		else
+			@active_player = player_1
+		end
+	end
+
+	def check_winner
+		if board.winner?(active_player.marker)
+			true
+		else
+			false
+		end
+	end
+
+	def get_player_1
+		puts """
+		Please select player 1 by entering a number below.
+		1 - Human
+		2 - Sequential Computer
+		3 - Random Computer
+		"""
+
+		@input1 = gets.chomp.to_i
+
+		if input1 == 1
+			@player_1 = Human.new('X')
+
+		elsif input1 == 2
+			@player_1 = Sequential.new('X')
+
+		elsif input1 == 3
+			@player_1 = RandomAI.new('X')
+
+		else
+			puts "Invalid input."
+			get_player_1
+		end
+	end
+
+	def get_player_2
+		puts """
+		Please select player 2 by entering a number below.
+		1 - Human
+		2 - Sequential Computer
+		3 - Random Computer
+		"""
+
+		@input2 = gets.chomp.to_i
+
+		if input2 == 1
+			@player_2 = Human.new('O')
+
+		elsif input2 == 2
+			@player_2 = Sequential.new('O')
+
+		elsif input2 == 3
+			@player_2 = RandomAI.new('O')
+
+		else
+			puts "Invalid input."
+			get_player_2
+		end
+	end
+end
