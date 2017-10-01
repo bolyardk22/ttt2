d99e596c69378df49f4109d06f6d2343f683d2a4
bolyardk22 <collinsk22@mycu.concord.edu>
2017-10-01 15:52:53 -0400

    everything works!

--- console_random_ai.rb
+++ console_random_ai.rb
@@ -9,17 +9,17 @@ class Random_AI
 		puts "It's #{marker}'s turn."
 
 		num_arr = [0,1,2,3,4,5,6,7,8]
+		shuff_arr = num_arr.shuffle
+		move = shuff_arr.pop
 
-		move = num_arr.sample
 		loop do
 			if ttt_board[move] == ''
 				move
 				break
 			else
+				move = shuff_arr.pop
 			end
 		end
-		
-		p "#{move}!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 		move
 	end
 end
